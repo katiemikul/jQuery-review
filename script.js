a9d0ee53ed6665c990731fe268ffa6ea19ff9f61
Katie Mikul <katie.heding@gmail.com>
2018-04-13 11:43:44 -0500

    add jquery

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('In jQuery Review!');
+$(document).ready(onReady);
+
+let i=0;
+
+function onReady(){
+    console.log('we are ready!');
+    $('#buttonThing').on('click', function(){
+        console.log('I was clicked!');
+        i++;
+        $('#counterThing').text(i);    
+    });
+    
+}
+
